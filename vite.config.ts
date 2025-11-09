ede1c4790519eb3237e3dc8daf399027f82afd42
PrittSpadeLord <pritt1999@gmail.com>
2025-11-09 22:36:41 +0530

    updated package versions; added some vite config to minify dist output

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import htmlMinifier  from 'vite-plugin-html-minifier'
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        htmlMinifier()
+    ],
+    build: {
+        outDir: 'dist',
+        sourcemap: false
+    }
 })
+
+//stuff related to code splitting and lazy loading here
